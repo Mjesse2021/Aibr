54de5c55dabd480825d67343ebb8277967820a25
Mjesse2021 <90251681+Mjesse2021@users.noreply.github.com>
2021-09-17 09:55:31 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+﻿                
+
+function openMenu() {
+    document.getElementById('mobile').style.width = '280px';
+}
+
+function closeSide() {
+    document.getElementById('mobile').style.width = '0';
+}
+
+
+
